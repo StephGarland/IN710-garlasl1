bd327fdc7ddc2865b4c85ffa317b839d23084ef5
StephGarland <garlasl1@student.op.ac.nz>
2016-03-03 19:10:57 +1300

    Add body to linked list class

---
+++ Practical 3.1 Chicken Shooter/RainbowChicken2016 Skeleton/RainbowChicken2016/PelletList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace RainbowChicken2016
+{
+    public class PelletList
+    {
+        Pellet headPointer = null;
+        Pellet tailPointer = null;
+
+        Rectangle boundsRectangle;
+
+        //==============================================================================
+        // Ctor
+        //==============================================================================
+        public PelletList(Rectangle boundsRectangle)
+        {
+            this.boundsRectangle = boundsRectangle;
+        }
+
+        //==============================================================================
+        // Add Pellet newPellet at the end of the list.
+        //==============================================================================
+        public void addPellet(Pellet newPellet)
+        {
+            if (headPointer == null)
+            {
+                headPointer = newPellet;
+                tailPointer = newPellet;
+            }
+            else
+            {
+                tailPointer.Next = newPellet;
+                tailPointer = newPellet;
+            }
+        }
+
+        //==============================================================================
+        // Walk the list, counting the number of Pellet. Return the count.
+        //==============================================================================
+        public int Count()
+        {
+            int count = 0;
+            Pellet current = headPointer;
+            while (current.Next != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+
+        //==============================================================================
+        // Walk the list, calling the Move() method of each Pellet
+        //==============================================================================
+        public void Move()
+        {
+            Pellet current = headPointer;
+            while (current.Next != null)
+            {
+                current.Move();
+                current = current.Next;
+            }
+        }
+
+        //==============================================================================
+        // Walk the list. For each Pellet, call TestOutOfBounds, passing boundsRectangle.
+        // For each node that is out of bounds, set its IsAlive property to false.
+        //==============================================================================
+        public void KillOutOfBounds()
+        {
+            Pellet current = headPointer;
+            while (current.Next != null)
+            {
+                if(current.TestOutOfBounds(boundsRectangle))
+                {
+                    current.IsAlive = false;
+                }
+                current = current.Next;
+            }
+        }
+
+        //==============================================================================
+        // Delete the argument Pellet pelletToDelete from the list.
+        // Be careful to maintain the integrity of the list, including
+        // headPointer and tailPointer.
+        //==============================================================================
+        public void DeleteOne(Pellet pelletToDelete)
+        {
+            Pellet current = headPointer;
+            while (current.Next != pelletToDelete)
+            {
+                current = current.Next;
+            }
+            current.Next = pelletToDelete.Next;
+        }
+
+        //==============================================================================
+        // Walk the list, deleting all nodes whose IsAlive propoerty is false
+        //==============================================================================
+        public void DeleteNotAlive()
+        {
+            Pellet current = headPointer;
+            while (current.Next != null)
+            {
+                if(current.IsAlive == false)
+                {
+                    DeleteOne(current);
+                }    
+                current = current.Next;
+            }
+        }
+
+        //==============================================================================
+        // Walk the list, calling each node's Draw method
+        //==============================================================================
+        public void Draw()
+        {
+            Pellet current = headPointer;
+            while (current.Next != null)
+            {
+                current.Draw();
+                current = current.Next;
+            }
+        }
+    }
+}
