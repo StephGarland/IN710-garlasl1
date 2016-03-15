f84c924e27441e64ca92c460f4f97e8b4e8bcc6c
StephGarland <garlasl1@student.op.ac.nz>
2016-03-15 14:55:20 +1300

    Add form and animal architecture

---
+++ Practical 5.1 Animal World/Practical 5.1 Animal World/Crocodile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_5._1_Animal_World
+{
+    class Crocodile : Animal
+    {
+        public Crocodile()
+        {
+            name = "Crocodile";
+            family = "Carnivore";
+            food = "Children";
+            image = new Bitmap("bison.jpg");
+        }
+    }
+}
---
+++ Practical 5.1 Animal World/Practical 5.1 Animal World/Koala.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_5._1_Animal_World
+{
+    class Koala : Animal
+    {
+        public Koala()
+        {
+            name = "Koala";
+            family = "Herbivore";
+            food = "Leaves";
+            image = new Bitmap("koala.jpg");
+        }
+    }
+}
