5e4817ee4e64769308cf53b1096e6342950434a0
StephGarland <garlasl1@student.op.ac.nz>
2016-05-13 08:05:32 +1200

    Create a table and insert a record

---
+++ Practical 11.1 Assignment Manager/Practical 11.1 Assignment Manager/CreateAssignmentDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_11._1_Assignment_Manager
+{
+    class CreateAssignmentDatabase
+    {
+        public CreateAssignmentDatabase()
+        {
+            string createTutorTable = @"CREATE TABLE tblTutor
+                                        (
+                                            tutorID int NOT NULL AUTO_INCREMENT,
+                                            lastName varchar(20) NOT NULL,
+                                            firstName varchar(20) NOT NULL,
+                                            email varchar(100),
+                                            PRIMARY KEY(tutorID)
+                                        );";
+
+            string insertQuery = @"INSERT INTO tblTutor (lastName,firstName,email)
+                                    VALUES('Lockhart', 'Gilderoy', 'lockhart.gilderoy@hw.ac.nz');";
+
+            string createPaperTable = @"CREATE TABLE tblPaper
+                                        (
+                                            paperID int NOT NULL AUTO_INCREMENT,
+                                            name varchar(50) NOT NULL,
+                                            tutorID int,
+                                            PRIMARY KEY(paperID),
+                                            FOREIGN KEY(tutorID) REFERENCES tblTutor(tutorID)
+                                        );";
+
+            string createAssignmentTable = @"CREATE TABLE tblAssignment
+                                            (
+                                                assignmentID int NOT NULL AUTO_INCREMENT,
+                                                name varchar(50) NOT NULL,
+                                                name varchar(50) NOT NULL,
+                                                dueDate datetime,
+                                                grade int NOT NULL,
+                                                paperID int,
+                                                PRIMARY KEY(assignmentID),
+                                                FOREIGN KEY(paperID) REFERENCES tblPaper(paperID)
+                                            );";
+        }
+    }
+}
--- Practical 11.1 Assignment Manager/Practical 11.1 Assignment Manager/Form1.cs
+++ Practical 11.1 Assignment Manager/Practical 11.1 Assignment Manager/Form1.cs
@@ -20,11 +20,31 @@ namespace Practical_11._1_Assignment_Manager
             bitdevConnection.ConnectionString = "Data Source = bitdev.ict.op.ac.nz;" +
                                                 "Initial Catalog = IN710_201601_GARLASL1;" +
                                                 "User ID = garlasl1;" +
-                                                "Password = PASSWORD;";
+                                                "Password = SGa_F845;";
+
+
+            string createTutorTable = @"CREATE TABLE tblTutor
+                                        (
+                                            tutorID int IDENTITY(1,1) PRIMARY KEY,
+                                            lastName varchar(20) NOT NULL,
+                                            firstName varchar(20) NOT NULL,
+                                            email varchar(100),
+                                        );";
+
+            string insertQuery = @"INSERT INTO tblTutor (lastName,firstName,email)
+                                    VALUES('Lockhart', 'Gilderoy', 'lockhart.gilderoy@hw.ac.nz');";
+
 
             bitdevConnection.Open();
-            
-            string selectQuery = "SELECT fireLongitude from dbo.tblFire";
+
+            //SqlCommand cmdCreateTutorTable = new SqlCommand(createTutorTable, bitdevConnection);
+            //cmdCreateTutorTable.ExecuteNonQuery();
+            //SqlCommand cmdInsertTutor = new SqlCommand(insertQuery, bitdevConnection);
+           // cmdInsertTutor.ExecuteNonQuery();
+
+
+            //"SELECT fireLongitude from dbo.tblFire"
+            string selectQuery = "SELECT lastName from dbo.tblTutor";
             SqlCommand worldCommand = new SqlCommand();
             worldCommand.Connection = bitdevConnection;
             worldCommand.CommandText = selectQuery;
@@ -33,7 +53,7 @@ namespace Practical_11._1_Assignment_Manager
             worldReader = worldCommand.ExecuteReader();
             while (worldReader.Read())
             {
-                listBox1.Items.Add(worldReader["fireLongitude"].ToString());
+                listBox1.Items.Add(worldReader["lastName"].ToString());
             }
 
             worldReader.Close();
